f244303a74dcf8df2fdf6a8e69d8168c17c64059
MMMMMMNM <wwwwwwwnm@outlook.com>
2026-08-25 18:03:25 +0800

    1

--- lua/index.lua
+++ lua/index.lua
@@ -1,12 +1,12 @@
-require("plugins.common")
+require("plugins.extra") -- function install and setup
 require("plugins.blink-cmp")
-require("plugins.ai")
-require("plugins.lsp_and_format")
-require("plugins.git")
-require("plugins.ui")
-require("plugins.explorer")
-require("plugins.markdown")
-require("plugins.overseer")
-require("plugins.comments")
+require("plugins.ai") -- ai setup
+require("plugins.lsp_and_format") -- lsp and format
+require("plugins.git") -- git setup
+require("plugins.ui") -- UI rendering
+require("plugins.explorer") -- file explorer
+require("plugins.markdown") -- md
+require("plugins.overseer") -- task manager
+require("plugins.comments") -- comments setup
 require("plugins.leetcode")
-require("plugins.dap")
+require("plugins.dap") -- debuger setup
